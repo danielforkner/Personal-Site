ed7fd26f3146b87ed290ec82f879ac38f88d8063
danielforkner <daniel.forkner@gmail.com>
2022-12-31 12:09:23 -0500

    footer and footer hide

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const Footer = () => {
+  const handleHide = () => {
+    const footer = document.querySelector('.footer');
+    footer.classList.add('footer-hide');
+  };
+
   return (
     <footer className="footer">
       <div className="footer-center">
         <div className="footer-text">Connect with me</div>
         <div className="footer-icons"> LinkedIn | GitHub | Email</div>
       </div>
-      <div className="footer-right">X</div>
+      <div onClick={handleHide} className="footer-right">
+        X
+      </div>
     </footer>
   );
 };
