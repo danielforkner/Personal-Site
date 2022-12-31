68dcca97240664ea6378f0f020a370aece402198
danielforkner <daniel.forkner@gmail.com>
2022-12-31 13:20:13 -0500

    hamburger menu WIP

--- src/components/Main/Aside.js
+++ src/components/Main/Aside.js
@@ -4,7 +4,9 @@ const Aside = () => {
   return (
     <>
       <div className="aside-toggle">
-        <div></div>
+        <span class="line line1"></span>
+        <span class="line line2"></span>
+        <span class="line line3"></span>
       </div>
       <aside>
         <nav>
