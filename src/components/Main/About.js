6eb47ec9e15919104de6e4d1134d9d4273027c1d
danielforkner <daniel.forkner@gmail.com>
2023-06-30 13:49:52 -0400

    AWS Certs

--- src/components/Main/About.js
+++ src/components/Main/About.js
@@ -6,9 +6,35 @@ const About = () => {
       <h2>About</h2>
       <p>
         Software engineer and web developer with a passion for elegant
-        functionality. Over 2years of experience utilizing technology to help
-        organizations pivot and scale.
+        functionality. Experience utilizing technology to help organizations
+        pivot and scale.
       </p>
+      <p style={{ marginTop: '1rem', fontWeight: 'bold' }}>
+        AWS Authorized Instructor (AAI)
+      </p>
+      <div style={{ display: 'flex', alignItems: 'center', marginTop: '1rem' }}>
+        <a
+          href="https://www.credly.com/badges/2ad382d3-8462-4d3d-b3d4-e4a1e1fc7d72"
+          target="_blank"
+          alt="AWS Cert"
+        >
+          <img src="/images/about/Cert-AAI.png" width="100" />
+        </a>
+        <a
+          href="https://www.credly.com/badges/ca61f1fc-3432-4580-825c-1205134b9857"
+          target="_blank"
+          alt="AWS Cert"
+        >
+          <img src="/images/about/Cert-SAA.png" width="100" />
+        </a>
+        <a
+          href="https://www.credly.com/badges/9f3ab7d5-2408-499e-bb86-7f8bcbb6d94e"
+          target="_blank"
+          alt="AWS Cert"
+        >
+          <img src="/images/about/Cert-CCP.png" width="100" />
+        </a>
+      </div>
       {/* <p>
         Recent projects include personal use, classroom demonstrations and aids,
         and more.
